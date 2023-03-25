73f521ba2457a5da43a34cadab1f272dd2379e5f
Costiuc Igor <90152687+Xenilae@users.noreply.github.com>
2023-03-25 17:33:00 +0200

    cleared layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import RequestTest from "./components/RequestTest"
 
 const App = () => {
 
   return (
     <div className="App">
-        <RequestTest />
     </div>
   )
 }
